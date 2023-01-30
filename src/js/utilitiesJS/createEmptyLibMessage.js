1e9230a7a2b7999ea44a7b0cd0c7ab6c3482d8aa
SerhiiNazarov <algazira89@gmail.com>
2023-01-30 23:35:16 +0200

    reload

--- src/js/utilitiesJS/createEmptyLibMessage.js
+++ src/js/utilitiesJS/createEmptyLibMessage.js
@@ -1,6 +1,6 @@
 export function createMessage() {
   const messageMarkup = `<div class="message-wrap" >
-      <p class="sorry">Sorry...</p>
+      <p class="sorry">Sorry....</p>
       <p class="message">
         You haven't added any movies yet. Hope you fix this soon :)
       </p>
